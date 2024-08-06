21fcf46d4b311fd67f3f914a83566154532451c3
Mo2men-dev <momensameh5@gmail.com>
2024-08-06 19:23:48 +0300

    task_4: done

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,6 +4,7 @@
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Waits for a random delay between 0 and max_delay
