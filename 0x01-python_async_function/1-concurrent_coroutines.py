ee1068a0f9698da6ab23f683e4a28db7ce247913
Mo2men-dev <momensameh5@gmail.com>
2024-08-06 19:02:32 +0300

    task_1: added typing

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 """ Takes int arg, waits for random delay """
 
+import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Waits for random delay max_delay n times, returns list of actual delays """
     delays: List[float] = []
     for i in range(n):
         delays.append(await wait_random(max_delay))
     
     return sorted(delays)
+
+
+print(asyncio.run(wait_n(5, 5)))
+print(asyncio.run(wait_n(10, 7)))
+print(asyncio.run(wait_n(10, 0)))
